97a5e9c22e4b3ad9018d1818a5ece67f162baa4a
Dea1Mak3r <134432395+Dea1Mak3r@users.noreply.github.com>
2025-11-24 15:38:05 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,29 +1,18 @@
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+        
         n = len(prices)
         
         # Если массив пустой или содержит один элемент, прибыли быть не может
         if n < 2:
             return 0
         
-        # Используем жадный алгоритм
-        # Отслеживаем минимальную цену покупки и максимальную прибыль
-        min_price = prices[0]  # минимальная цена для покупки
-        max_profit = 0  # максимальная прибыль
+        min_price = prices[0]
+        max_profit = 0
         
-        # Проходим по всем ценам начиная со второго дня
         for i in range(1, n):
-            # Обновляем минимальную цену покупки
             min_price = min(min_price, prices[i])
-            
-            # Вычисляем потенциальную прибыль от продажи в текущий день
-            current_profit = prices[i] - min_price
-            
-            # Обновляем максимальную прибыль
+            current_profit = prices[i] - min_price            
             max_profit = max(max_profit, current_profit)
         
         return max_profit
